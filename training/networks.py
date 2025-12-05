7a01cf778e6c552e6e4a2e371f41c298f6177636
jeh15 <jeh15@fsu.edu>
2025-12-05 13:22:58 -0500

    Update

--- training/networks.py
+++ training/networks.py
@@ -91,73 +91,44 @@ class MLP(nnx.Module):
         for layer in self.layers:
             x = layer(x)
         return x
+    
+class Policy(nnx.Module):
+    def __init__(
+        self,
+        input_size: types.ObservationSize,
+        output_size: int,
+        input_normalization_fn: types.InputNormalizationFn = types
+        .identity_normalization_fn,
+        layer_sizes: Sequence[int] = (256, 256),
+        activation: ActivationFn = nn.tanh,
+        kernel_init: Initializer = jax.nn.initializers.lecun_uniform(),
+        bias: bool = True,
+        layer_normalization: bool = False,
+        observation_key: str = "state",
+    ):
+        self.input_normalization_fn = input_normalization_fn
+        self.observation_key = observation_key
+        self.squeeze_output = output_size == 1
 
-def make_policy_network(
-    input_size: types.ObservationSize,
-    output_size: int,
-    input_normalization_fn: types.InputNormalizationFn = types
-    .identity_normalization_fn,
-    layer_sizes: Sequence[int] = (256, 256),
-    activation: ActivationFn = nn.tanh,
-    kernel_init: Initializer = jax.nn.initializers.lecun_uniform(),
-    bias: bool = True,
-    layer_normalization: bool = False,
-    observation_key: str = "state",
-) -> FeedForwardNetwork:
-    """Intializes a policy network."""
-    features = [input_size] + list(layer_sizes) + [output_size]
-    policy_network = MLP(
-        features=features,
-        activation=activation,
-        kernel_init=kernel_init,
-        bias=bias,
-        layer_normalization=layer_normalization,
-    )
+        observation_size = _get_observation_size(
+            input_size, observation_key
+        )
 
-    def apply(normalization_params, policy_params, x):
-        x = input_normalization_fn(x, normalization_params)
-        x = x if isinstance(x, jnp.ndarray) else x[observation_key]
-        return policy_network.apply(policy_params, x)
+        self.normalization_params = nnx.BatchStat(jnp.zeros((observation_size)))
 
-    observation_size = _get_observation_size(
-        input_size, observation_key
-    )
-    dummy_input = jnp.zeros((1, observation_size))
-    return FeedForwardNetwork(
-        init=lambda key: policy_network.init(key, dummy_input), apply=apply,
-    )
+        features = [input_size] + list(layer_sizes) + [output_size]
+        self.network = MLP(
+            features=features,
+            activation=activation,
+            kernel_init=kernel_init,
+            bias=bias,
+            layer_normalization=layer_normalization,
+        )
 
-
-def make_value_network(
-    input_size: types.ObservationSize,
-    input_normalization_fn: types.InputNormalizationFn = types
-    .identity_normalization_fn,
-    layer_sizes: Sequence[int] = (256, 256),
-    activation: ActivationFn = nn.tanh,
-    kernel_init: Initializer = jax.nn.initializers.lecun_uniform(),
-    bias: bool = True,
-    layer_normalization: bool = False,
-    observation_key: str = "state",
-) -> FeedForwardNetwork:
-    """Intializes a value network."""
-    features = [input_size] + list(layer_sizes) + [1]
-    value_network = MLP(
-        features=features,
-        activation=activation,
-        kernel_init=kernel_init,
-        bias=bias,
-        layer_normalization=layer_normalization,
-    )
-
-    def apply(normalization_params, value_params, x):
-        x = input_normalization_fn(x, normalization_params)
-        x = x if isinstance(x, jnp.ndarray) else x[observation_key]
-        return jnp.squeeze(value_network.apply(value_params, x), axis=-1)
-
-    observation_size = _get_observation_size(
-        input_size, observation_key
-    )
-    dummy_input = jnp.zeros((1, observation_size))
-    return FeedForwardNetwork(
-        init=lambda key: value_network.init(key, dummy_input), apply=apply,
-    )
+    def __call__(self, x: jax.Array):
+        x = x if isinstance(x, jnp.ndarray) else x[self.observation_key]
+        x = self.input_normalization_fn(x, self.normalization_params)
+        out = self.network(x)
+        if self.squeeze_output:
+            out = jnp.squeeze(out, axis=-1)
+        return out
