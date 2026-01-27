b47abac9e7e11eb153c25abb4a12be8cc9ebb69c
jeh15 <jeh15@fsu.edu>
2026-01-27 10:30:38 -0500

    Retrain with out joint domain randomization for isolation.

--- training/envs/unitree_go2/randomize.py
+++ training/envs/unitree_go2/randomize.py
@@ -16,29 +16,12 @@ def domain_randomize(
         FLOOR_BODY_ID = 0
         TORSO_BODY_ID = 1
 
-        # Number of joints (excluding floating base):
-        num_joints = (mjx_model.nv - 6)
-
         # Floor Friction:
         rng, key = jax.random.split(rng)
         geom_friction = jax.random.uniform(key, minval=0.6, maxval=1.2)
         friction = mjx_model.geom_friction.at[FLOOR_BODY_ID, 0].set(
             geom_friction,
         )
-
-        # Joint Friction:
-        rng, key = jax.random.split(rng)
-        frictionloss = mjx_model.dof_frictionloss[6:] * jax.random.uniform(
-            key, shape=(num_joints,), minval=0.9, maxval=1.1,
-        )
-        dof_frictionloss = mjx_model.dof_frictionloss.at[6:].set(frictionloss)
-
-        # Armature:
-        rng, key = jax.random.split(rng)
-        armature = mjx_model.dof_armature[6:] * jax.random.uniform(
-            key, shape=(num_joints,), minval=1.0, maxval=1.05,
-        )
-        dof_armature = mjx_model.dof_armature.at[6:].set(armature)
 
         # Center of Mass offset:
         rng, key = jax.random.split(rng)
@@ -65,49 +48,29 @@ def domain_randomize(
             mjx_model.body_mass[TORSO_BODY_ID] + delta
         )
 
-        # Joint reference randomization:
-        rng, key = jax.random.split(rng)
-        qpos0 = mjx_model.qpos0
-        delta = jax.random.uniform(
-            key, shape=(num_joints,), minval=-0.05, maxval=0.05,
-        )
-        qpos0 = qpos0.at[7:].set(qpos0[7:] + delta)
-
         return (
             friction,
-            dof_frictionloss,
-            dof_armature,
             body_ipos,
             body_mass,
-            qpos0,
         )
 
     (
         friction,
-        dof_frictionloss,
-        dof_armature,
         body_ipos,
         body_mass,
-        qpos0,
     ) = randomize_parameters(rng)
 
     in_axes = jax.tree.map(lambda x: None, mjx_model)
     in_axes = in_axes.tree_replace({
         'geom_friction': 0,
-        'dof_frictionloss': 0,
-        'dof_armature': 0,
         'body_ipos': 0,
         'body_mass': 0,
-        'qpos0': 0,
     })
 
     mjx_model = mjx_model.tree_replace({
         'geom_friction': friction,
-        'dof_frictionloss': dof_frictionloss,
-        'dof_armature': dof_armature,
         'body_ipos': body_ipos,
         'body_mass': body_mass,
-        'qpos0': qpos0,
     })  # type: ignore
 
     return mjx_model, in_axes
