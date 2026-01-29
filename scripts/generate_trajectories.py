7c0195d52ccac2133c1de7f0ab956469409c29a4
jeh15 <jeh15@fsu.edu>
2026-01-28 19:53:45 -0500

    Added IK Solver to generate safe motions.

--- scripts/generate_trajectories.py
+++ scripts/generate_trajectories.py
@@ -9,6 +9,8 @@ import jax.numpy as jnp
 import numpy as np
 
 import mujoco
+from mujoco import mjx
+import optax
 
 jax.config.update('jax_enable_x64', True)
 
@@ -46,109 +48,228 @@ def main(argv=None):
     mj_model = mujoco.MjModel.from_xml_path(
         filepath,
     )
+    mjx_model = mjx.put_model(mj_model)
     control_rate = 0.02
 
     num_time_steps = 500
     max_switches = 10
     trajectory_time = num_time_steps * control_rate
-    period_lb, period_ub = 1.0, 10.0
-    frequency_lb = 2 * np.pi / period_ub
-    frequency_ub = 2 * np.pi / period_lb
-
-    # Get Joint Limits:
-    joint_lb, joint_ub = mj_model.jnt_range.T
-    num_joints = joint_lb.shape[0]
-
-    # Soft Joint Limits:
-    limit_factor = 0.5
-    joint_center = (joint_lb + joint_ub) / 2
-    joint_range = joint_ub - joint_lb
-    soft_range = limit_factor * joint_range
-    soft_lb = joint_center - soft_range
-    soft_ub = joint_center + soft_range
 
     # Probability of Step Function Trajectory
-    step_function_prob = 0.9
+    step_function_prob = 1.0
+    minimum_duration_between_steps = 1.0
+    minimum_step_duration = int(
+        minimum_duration_between_steps / control_rate
+    )
 
-    def generate_sinusoid_trajectory(
+    # Model Site IDs and Home Position:
+    home_position = jnp.array(mj_model.keyframe('home').qpos)
+    base_id = mujoco.mj_name2id(
+        mj_model, mujoco.mjtObj.mjOBJ_BODY.value, 'base_link'
+    )
+    feet_site = [
+        'front_right_foot',
+        'front_left_foot',
+        'hind_right_foot',
+        'hind_left_foot',
+    ]
+    feet_site_idx = [
+        mujoco.mj_name2id(mj_model, mujoco.mjtObj.mjOBJ_SITE.value, f)
+        for f in feet_site
+    ]
+    assert not any(id_ == -1 for id_ in feet_site_idx), 'Site not found.'
+    foot_site_ids = np.array(feet_site_idx)
+
+    # Initialize IK Optimizer:
+    learning_rate = 0.05
+    optimizer = optax.adam(learning_rate)
+
+    def generate_ik_trajectory(
         key: jax.Array,
         num_time_steps: int = 500,
-    ) -> jax.Array:
-        key, offset_key, amplitude_key, frequency_key = jax.random.split(key, 4)
-
-        # Generate Sinusoid Parameters
-        offset = jax.random.uniform(
-            offset_key, shape=(num_joints,), minval=soft_lb, maxval=soft_ub,
-        )
-        amplitude = jax.random.uniform(
-            amplitude_key, shape=(num_joints,), minval=-soft_range, maxval=soft_range,
-        )
-        frequency = jax.random.uniform(
-            frequency_key, shape=(num_joints,), minval=frequency_lb, maxval=frequency_ub,
-        )
-
-        # Generate Trajectory
-        x = jnp.linspace(0, trajectory_time, num_time_steps)[:, None]
-        p_offset = offset[None, :]
-        p_amp = amplitude[None, :]
-        p_freq = frequency[None, :]
-        trajectory = p_offset + p_amp * jnp.sin(p_freq * x)
-
-        # Clip to Joint Limits:
-        trajectory = jnp.clip(trajectory, soft_lb, soft_ub)
-
-        return trajectory
-
-    def generate_step_trajectory(
-        key: jax.Array,
-        num_time_steps: int = 500,
-        max_switches: int = 10,
     ) -> jnp.ndarray:
-        k_count, k_time, k_val = jax.random.split(key, 3)
+        """
+        Generates a joint-space trajectory (T, n_joints) by tracking
+        safe random Cartesian targets using Gradient Descent IK.
+        """
 
-        # Calculate number of switches:
-        n_switches = jax.random.randint(
-            k_count, shape=(), minval=1, maxval=max_switches + 1
+        # Task Space Safe Zones for Feet:
+        half_size = jnp.array([0.2, 0.1, 0.15])
+        center_wrt_base = jnp.array([
+            [0.2, -0.15, -0.2,],    # Front Right
+            [0.2,  0.15, -0.2,],    # Front Left
+            [-0.2, -0.15, -0.2,],   # Rear Right
+            [-0.2,  0.15, -0.2,],   # Rear Left
+        ])
+        lb = center_wrt_base - half_size[None, :]   # (4, 3) Lower Bounds
+        ub = center_wrt_base + half_size[None, :]   # (4, 3) Upper Bounds
+        bound_range = ub - lb
+
+        def generate_step_targets(
+            rng: jax.Array, max_switches: int = 10, minimum_step_duration: int = 20,
+        ) -> jnp.ndarray:
+            key_switches, key_deltas, key_values = jax.random.split(rng, 3)
+
+            # Random number of switches:
+            n_switches = jax.random.randint(
+                key_switches, shape=(), minval=1, maxval=max_switches + 1,
+            )
+
+            # Generate random gaps
+            deltas = jax.random.randint(
+                key_deltas,
+                shape=(max_switches,),
+                minval=0,
+                maxval=100,
+            )
+
+            # Enforce Minimum Duration:
+            safe_deltas = deltas + minimum_step_duration
+
+            # Convert Deltas to Timestamps:
+            generated_times = jnp.cumsum(safe_deltas)
+
+            # Masking Logic:
+            idx = jnp.arange(max_switches)
+            count_mask = idx < n_switches
+
+            # Filter by Maximum Time:
+            time_mask = generated_times < num_time_steps
+
+            # Combine masks: Valid if index < N AND time < MaxTime
+            valid_mask = count_mask & time_mask
+
+            # Active Switch Times:
+            active_switch_times = jnp.where(
+                valid_mask, generated_times, num_time_steps + 1,
+            )
+
+            # Generate Values:
+            param_shape = (max_switches + 1, *lb.shape)
+            values = jax.random.uniform(
+                key_values, shape=param_shape, minval=lb, maxval=ub
+            )
+
+            # Task Space Trajectory:
+            t = jnp.arange(num_time_steps)
+            indices = jnp.searchsorted(active_switch_times, t, side='right')
+            trajectory = values[indices]
+
+            return trajectory
+
+        def generate_sinusoidal_targets(key: jax.Array) -> jnp.ndarray:
+            key, frequency_key, phase_key, amplitude_key, center_key = jax.random.split(key, 5)
+            period_lb, period_ub = 0.5, 10.0
+            frequency_lb, frequency_ub = 2 * jnp.pi / period_ub, 2 * jnp.pi / period_lb
+            frequency = jax.random.uniform(
+                frequency_key, shape=(4, 3), minval=frequency_lb, maxval=frequency_ub,
+            )
+            phase = jax.random.uniform(
+                phase_key, shape=(4, 3), minval=0.0, maxval=2*jnp.pi,
+            )
+            amplitude = jax.random.uniform(
+                amplitude_key, shape=(4, 3), minval=-bound_range, maxval=bound_range,
+            )
+            centers = jax.random.uniform(
+                center_key, shape=(4, 3), minval=lb, maxval=ub,
+            )
+
+            # Create Task Space Trajectory
+            t = jnp.linspace(0, trajectory_time, num_time_steps)[:, None, None]
+            targets = centers[None, :, :] + amplitude[None, :, :] * jnp.sin(
+                frequency[None, :, :] * t + phase[None, :, :]
+            )
+
+            return targets
+
+        # Choose Target Generation Method
+        key, target_key, bernoulli_key = jax.random.split(key, 3)
+        step_targets = generate_step_targets(target_key, max_switches=max_switches, minimum_step_duration=minimum_step_duration)
+        sinusoidal_targets = generate_sinusoidal_targets(target_key)
+        mask = jax.random.bernoulli(bernoulli_key, p=step_function_prob)
+        targets = jnp.where(
+            mask,
+            step_targets,
+            sinusoidal_targets,
         )
 
-        # Generate the Switch Times
-        times = jax.random.randint(
-            k_time, shape=(max_switches,), minval=1, maxval=num_time_steps
-        )
-        sorted_times = jnp.sort(times)
+        # Clip to Safe Zones:
+        targets = jnp.clip(targets, lb[None, ...], ub[None, ...])
 
-        idx = jnp.arange(max_switches)
-        mask = idx < n_switches
+        # Inverse Kinematics:
+        def fk_fn(q):
+            # Update Physics
+            d = mjx.make_data(mjx_model)
+            d = d.replace(qpos=q)
+            d = mjx.kinematics(mjx_model, d)
 
-        active_switch_times = jnp.where(mask, sorted_times, num_time_steps + 1)
+            # Positions in World Frame
+            feet_pos = d.site_xpos[foot_site_ids]
+            base_pos = d.xpos[base_id]
 
-        # Generate the Setpoint Values
-        param_shape = (max_switches + 1, soft_lb.shape[0])
-        values = jax.random.uniform(
-            k_val, shape=param_shape, minval=soft_lb, maxval=soft_ub
-        )
+            # Position Relative to Base
+            return feet_pos - base_pos
 
-        # Build the Trajectory:
-        t = jnp.arange(num_time_steps)
-        indices = jnp.searchsorted(active_switch_times, t, side='right')
-        trajectory = values[indices]
+        # Loss Function: Distance to Target + Regularization
+        def ik_loss(q, target_pos):
+            current_pos = fk_fn(q)
+            dist_error = jnp.sum((current_pos - target_pos) ** 2)
+            reg_error = jnp.sum((q - home_position) ** 2)
+            return dist_error + 0.05 * reg_error
 
-        return trajectory
+        # Gradient Step
+        grad_fn = jax.value_and_grad(ik_loss)
+
+        def solve_step(carry_q, target):
+            """
+                Solves IK for a single timestep using an inner optimization loop.
+                carry_q: The solution from the previous timestep (Warm start)
+                target: The Cartesian target for the current timestep
+            """
+
+            # Initialize Optimizer State for this timestep
+            opt_state = optimizer.init(carry_q)
+
+            # Inner Optimization Loop
+            def optimization_loop(carry, unused_t):
+                q, opt_state = carry
+
+                # Calculate Gradients
+                loss, grads = grad_fn(q, target)
+                updates, opt_state = optimizer.update(grads, opt_state, params=q)
+                q_new = optax.apply_updates(q, updates)
+
+                # Project back to Joint Limits (Safety)
+                q_new = jnp.clip(
+                    q_new, mj_model.jnt_range[:, 0], mj_model.jnt_range[:, 1],
+                )
+                return (q_new, opt_state), None
+
+            (q_solved, _), _ = jax.lax.scan(
+                optimization_loop,
+                init=(carry_q, opt_state),
+                xs=None,
+                length=50,
+            )
+
+            # Return the result for the trajectory scan
+            return q_solved, q_solved
+
+        # Warm start and run scan:
+        init_q = home_position
+        final_q, q_trajectory = jax.lax.scan(solve_step, init_q, targets)
+
+        return q_trajectory
 
     # Generate Trajectories:
     def loop(carry, unused_t):
         key = carry
-        key, subkey = jax.random.split(key)
-        mask = jax.random.bernoulli(subkey, p=step_function_prob)
 
         # Generate Trajectory Based on Mask
         key, trajectory_key = jax.random.split(key)
-        trajectory = jax.lax.cond(
-            mask,
-            functools.partial(generate_step_trajectory, num_time_steps=num_time_steps, max_switches=max_switches),
-            functools.partial(generate_sinusoid_trajectory, num_time_steps=num_time_steps),
-            trajectory_key,
-        )
+        trajectory = functools.partial(
+            generate_ik_trajectory, num_time_steps=num_time_steps,
+        )(trajectory_key)
 
         return key, trajectory
 
@@ -163,7 +284,6 @@ def main(argv=None):
     # Play trajectories to check of collisions:
     collisions = 0
     for i, trajectory in enumerate(trajectories):
-        # Initialize Mujoco Data:
         data = mujoco.MjData(mj_model)
         for t in range(num_time_steps):
             data.qpos = np.array(trajectory[t])
@@ -201,7 +321,11 @@ def main(argv=None):
         data_directory,
         f'{FLAGS.output_filename}.csv',
     )
-    np.savetxt(output_path, np.array(trajectories))
+
+    # Flatten and Save: (Trials, Time, Joints) -> (Trials * Time, Joints)
+    num_joints = trajectories.shape[-1]
+    flattened_data = np.reshape(np.array(trajectories), (-1, num_joints))
+    np.savetxt(output_path, flattened_data)
 
 
 if __name__ == '__main__':
